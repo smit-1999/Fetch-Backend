d6a723895a6b1017aa8854a23cccfe539fa52caf
Smit Shah <smitshah1999@gmail.com>
2023-10-01 23:29:00 -0500

    handle deductions and remove print statements

--- app.py
+++ app.py
@@ -1,12 +1,57 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request
 import sqlite3
 from sqlite3 import Error
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return 'Hello, World!'
+def handle_deduction(req):
+    points =  - 1 * req['points']
+    payer = req['payer']
+    conn = sqlite3.connect('./database.db')
+    try:
+        c = conn.cursor()
+        c.execute("""SELECT SUM(points) as total FROM Points WHERE payer = ?""", (payer,))
+
+        total = c.fetchall()
+        if total[0][0] < points :
+            return "No points available to subtract", 500
+
+        c.execute("""
+            WITH PrefixSum AS ( SELECT *, SUM(points) OVER (ORDER BY timestamp) AS totalPoints FROM Points WHERE payer = ?) 
+            SELECT * FROM ( 
+                SELECT id, payer, points,timestamp FROM PrefixSum WHERE totalPoints<= ? AND points>0 ORDER BY timestamp
+            )
+            UNION     
+            SELECT * FROM (
+                SELECT  id, payer, points,timestamp FROM PrefixSum WHERE totalPoints>= ? AND points>0 ORDER BY timestamp LIMIT 1
+            )
+            ORDER BY timestamp;""",(payer,points,points))
+        
+        allPossiblePayers = c.fetchall()
+        updated_rows = []
+        for row in allPossiblePayers:
+            if points <= 0 or row[2] <= 0:
+                updated_rows.append([row[0],row[1],row[2],row[3]])
+            else:
+                deduct_points = min(points, row[2])
+                updatedPoints = row[2] - deduct_points
+                points -= deduct_points
+                updated_rows.append([row[0],row[1],updatedPoints,row[3]])
+
+        for row in updated_rows:
+            c.execute("""UPDATE Points
+                    set points = ?
+                    WHERE id = ?; """,(row[2], row[0]))
+            conn.commit()
+        
+        conn.close()
+        return "Successfully added negative point",200
+    except Error as e:
+        print(e)
+        return e,500
+    finally:
+        if conn:
+            conn.close()
 
 @app.route('/add', methods=['POST'])
 def add_to_points():
@@ -24,6 +69,10 @@ def add_to_points():
     points=json['points']
     timestamp=json['timestamp']
 
+    if points < 0:
+        return handle_deduction(json)
+
+    print('is error here')
     conn = sqlite3.connect('./database.db')
     try:
         c = conn.cursor()
@@ -70,14 +119,12 @@ def spend_points():
         return "Missing points parameter",500
     
     to_spend = req['points']
-    print(req)
     conn = sqlite3.connect('./database.db')
     c = conn.cursor()
     c.execute("SELECT SUM(points) as totalUserPoints FROM points")
     totalUserPoints = 0
     myresult = c.fetchall()
     for x in myresult:
-        print(x)
         totalUserPoints = x[0]
     
     if totalUserPoints < to_spend:
@@ -90,21 +137,11 @@ def spend_points():
               )
             UNION
             SELECT * FROM (
-                SELECT 
-                    id, 
-                    payer, 
-                    points,
-                    timestamp
-                FROM PrefixSum
-                WHERE totalPoints>=? AND points>0
-                ORDER BY timestamp
-                LIMIT 1
+                SELECT id, payer, points,timestamp FROM PrefixSum WHERE totalPoints>=? AND points>0 ORDER BY timestamp LIMIT 1
             )
             ORDER BY timestamp""",(to_spend,to_spend))
     allPossiblePayers = c.fetchall()
-    for x in allPossiblePayers:
-        print('allPossiblePayers',x)
-    
+
     res={}
     updated_rows = []
     for row in allPossiblePayers:
@@ -119,7 +156,7 @@ def spend_points():
             else:
                 res[row[1]] = -deduct_points
             updated_rows.append([row[0],row[1],updatedPoints,row[3]])
-    # print('res:',res,'urow:',updated_rows)
+
     for row in updated_rows:
         c.execute("""UPDATE Points
                 set points = ?
@@ -128,6 +165,7 @@ def spend_points():
     
     conn.close()
     return res,200 
+
 if __name__ == "__main__":
 
     """ create a database connection to a SQLite database """
@@ -135,17 +173,16 @@ if __name__ == "__main__":
         conn = sqlite3.connect('./database.db')
         c = conn.cursor()
         c.execute('''
-                CREATE TABLE IF NOT EXISTS points
-                ([id] INTEGER PRIMARY KEY AUTOINCREMENT,
-                [payer] TEXT,
-                [points] INTEGER,
-                [timestamp] DATETIME)
+                    CREATE TABLE IF NOT EXISTS points
+                    ([id] INTEGER PRIMARY KEY AUTOINCREMENT,
+                    [payer] TEXT,
+                    [points] INTEGER,
+                    [timestamp] DATETIME)
                 ''')
         conn.commit()
     except Error as e:
-        print('Exception found',e)
+        print(e)
     finally:
         if conn:
-            print('closed conn in main')
             conn.close()
     app.run(host='127.0.0.1',port=8000)  
