a0abbcd44f6b3d1df35981c4e6c16bceff4c7775
Smit Shah <smitshah1999@gmail.com>
2023-10-01 21:46:59 -0500

    add route for spending points

--- app.py
+++ app.py
@@ -63,6 +63,71 @@ def check_balance():
         a=1
     return res
 
+@app.route('/spend', methods=['POST'])
+def spend_points():
+    req = request.get_json()
+    if 'points' not in req:
+        return "Missing points parameter",500
+    
+    to_spend = req['points']
+    print(req)
+    conn = sqlite3.connect('./database.db')
+    c = conn.cursor()
+    c.execute("SELECT SUM(points) as totalUserPoints FROM points")
+    totalUserPoints = 0
+    myresult = c.fetchall()
+    for x in myresult:
+        print(x)
+        totalUserPoints = x[0]
+    
+    if totalUserPoints < to_spend:
+        return  "User does not have enough points to spend", 400
+    
+    c.execute("""
+              WITH PrefixSum AS ( SELECT *, SUM(points) OVER (ORDER BY timestamp) AS totalPoints FROM Points) 
+            SELECT * FROM ( 
+              SELECT id, payer, points,timestamp FROM PrefixSum WHERE totalPoints<=? AND points>0 ORDER BY timestamp
+              )
+            UNION
+            SELECT * FROM (
+                SELECT 
+                    id, 
+                    payer, 
+                    points,
+                    timestamp
+                FROM PrefixSum
+                WHERE totalPoints>=? AND points>0
+                ORDER BY timestamp
+                LIMIT 1
+            )
+            ORDER BY timestamp""",(to_spend,to_spend))
+    allPossiblePayers = c.fetchall()
+    for x in allPossiblePayers:
+        print('allPossiblePayers',x)
+    
+    res={}
+    updated_rows = []
+    for row in allPossiblePayers:
+        if to_spend <= 0 or row[2] <= 0:
+            updated_rows.append([row[0],row[1],row[2],row[3]])
+        else:
+            deduct_points = min(to_spend, row[2])
+            updatedPoints = row[2] - deduct_points
+            to_spend -= deduct_points
+            if row[1] in res:
+                res[row[1]] += -deduct_points
+            else:
+                res[row[1]] = -deduct_points
+            updated_rows.append([row[0],row[1],updatedPoints,row[3]])
+    # print('res:',res,'urow:',updated_rows)
+    for row in updated_rows:
+        c.execute("""UPDATE Points
+                set points = ?
+                WHERE id = ? AND payer = ?; """,(row[2], row[0], row[1]))
+        conn.commit()
+    
+    conn.close()
+    return res,200 
 if __name__ == "__main__":
 
     """ create a database connection to a SQLite database """
